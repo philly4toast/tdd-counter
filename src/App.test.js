f40a36b81ceaec9d9dadfea8fa2d8e74469868dc
philly4toast <phillipyoun@gmail.com>
2020-06-18 22:38:41 -0700

    Configure and shallow enzyme?

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//must do for every test file we create
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+//
+
+describe('Counter Testing', ()=>{
+  test('render the title of counter', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText('This is counter app');
+    expect(linkElement).toBeInTheDocument();
+})
 });
